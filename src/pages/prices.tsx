b9b182543bb4ee59a67549650c055f17c46f558b
Hamilton <>
2023-07-20 13:12:54 +0200

    add empty prices page

---
+++ src/pages/prices.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import { HeadFC, PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout/Layout";
+import { Phone, Instagram, Facebook } from "lucide-react";
+
+const Prices: FC<PageProps> = () => {
+  return (
+    <Layout>
+      <main>
+
+      </main>
+    </Layout>
+  );
+};
+
+export default Prices;
+export const Head: HeadFC = () => <title>Prices & Availibility</title>;
